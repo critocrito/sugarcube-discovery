334afbfc77488847d1211ceaa55ab67000f3165d
Christo <christo@cryptodrunks.net>
2019-12-01 00:25:27 +0800

    refactor: beautified content header

--- src/js/components/ContentHeader.tsx
+++ src/js/components/ContentHeader.tsx
@@ -11,54 +11,53 @@ interface ContentHeaderProps {
 const ContentHeader = ({url}: ContentHeaderProps) => {
   const {type, platform} = detectContent(url);
 
+  const iconSize = 28;
+
   let desc;
   let icon;
   switch (type) {
     case "youtube_video": {
       desc = "Youtube Video";
-      icon = <Youtube size={34} />;
+      icon = <Youtube size={iconSize} />;
       break;
     }
     case "youtube_channel": {
       desc = "Youtube Channel";
-      icon = <Youtube size={34} />;
+      icon = <Youtube size={iconSize} />;
       break;
     }
     case "twitter_tweet": {
       desc = "Twitter Tweet";
-      icon = <Twitter size={34} />;
+      icon = <Twitter size={iconSize} />;
       break;
     }
     case "twitter_user": {
       desc = "Twitter Feed";
-      icon = <Twitter size={34} />;
+      icon = <Twitter size={iconSize} />;
       break;
     }
     case "facebook_post": {
       desc = "Facebook Post";
-      icon = <Facebook size={34} />;
+      icon = <Facebook size={iconSize} />;
       break;
     }
     default:
       desc = "Website Archival";
-      icon = <Code size={34} />;
+      icon = <Code size={iconSize} />;
   }
 
   return (
-    <div className="flex flex-column w-100">
-      <div
-        data-testid="color-coding"
-        className={c("h2 w-100", `bg-${platform}`)}
-      >
-        &nbsp;
-      </div>
-      <div className="w-100 pl3 pr3 mt1 flex items-center">
+    <div
+      data-testid="color-coding"
+      className={c("flex flex-column w-100 white pa3", `bg-${platform}`)}
+    >
+      <div className="w-100 mt1 flex items-start">
         <span>{icon}</span>
         <h2 className="f3 lh-title ttu tracked ml3 mt0 mb0 flex items-center">
           {desc}
         </h2>
       </div>
-      <span className="f6 lh-copy i pl3 pr3">{url}</span>
+      <span className="f6 lh-copy i">{url}</span>
     </div>
   );
 };
